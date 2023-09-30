210aee5096653723a8be022455be8c27aec9db87
ibrahimeatspie <104ibrahimshah@gmail.com>
2023-09-29 17:41:02 -0700

    commented out spam

--- pose_estimation.py
+++ pose_estimation.py
@@ -92,7 +92,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
             # calculate angle between relevant points
             angle = calculate_angle(a, b, c)
-            print(angle)
+            #print(angle)
             time.sleep(0.5)
             # visualize angle
 
